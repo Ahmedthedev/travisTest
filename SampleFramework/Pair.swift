f1259acb07f67e1a40e8366027379b2675a6edee
Avatar <ahmedhachemi1994@gmail.com>
2018-01-17 15:05:53 +0100

    init

---
+++ SampleFramework/Pair.swift
@@ -0,0 +1,17 @@
+//
+//  Pair.swift
+//  SampleFramework
+//
+//  Created by apple on 17/01/2018.
+//  Copyright © 2018 Ahmed. All rights reserved.
+//
+
+import Foundation
+
+extension Int {
+    
+    public var isPair: Bool {
+        return self % 2 == 0
+    }
+    
+}
